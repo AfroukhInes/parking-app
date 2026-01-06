49e142c21cd15d2d129badbd009b2ce59f6f4f91
Afroukh Ines <ines.afroukh.business@gmail.com>
2026-01-06 13:21:21 +0100

    Update axios.js

--- parking-frontend/src/api/axios.js
+++ parking-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://parking-backend:5000"
+  baseURL: "http://18.212.78.31:5000"
 });
 
 api.interceptors.request.use((config) => {
